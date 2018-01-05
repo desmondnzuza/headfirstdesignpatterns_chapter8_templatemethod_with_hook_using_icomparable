5ade9ad5b2ac423b51107e377311dcfb319f9d0d
desmondnzuza <desmondnzuza@gmail.com>
2018-01-05 11:27:29 +0200

    Adds initial implementation of th template method patter using IComparable

---
+++ src/DuckSortTestDrive/Duck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DuckSortTestDrive
+{
+    class Duck : IComparable
+    {
+        private readonly string _name;
+        private readonly int _weight;
+
+        public Duck(string name, int weight)
+        {
+            _name = name;
+            _weight = weight;
+        }
+
+        public int GetWeight()
+        {
+            return _weight;
+        }
+
+        public override string ToString()
+        {
+            return $"{_name} weighs {_weight}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            Duck otherDuck = (Duck) obj;
+
+            if (_weight < otherDuck.GetWeight())
+            {
+                return -1;
+            }
+            else if (_weight == otherDuck._weight)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
---
+++ src/DuckSortTestDrive/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DuckSortTestDrive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck[] ducks =
+            {
+                new Duck("Daffy", 8),
+                new Duck("Dewey", 2),
+                new Duck("Howard", 7),
+                new Duck("Louie", 2),
+                new Duck("Donald", 10),
+                new Duck("Huey", 2),
+            };
+
+            Console.WriteLine("Before sorting:");
+            Display(ducks);
+
+            Array.Sort(ducks);
+
+            Console.WriteLine("\nAfter sorting:");
+            Display(ducks);
+
+            Console.ReadLine();
+        }
+
+        private static void Display(Duck[] ducks)
+        {
+            for (int i = 0; i < ducks.Length; i++)
+            {
+                Console.WriteLine(ducks[i]);
+            }
+        }
+    }
+}
